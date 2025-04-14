12af5e9d10802f8ccdee21d1184f48ba660021cf
Jiri <godfreyjavier88@gmail.com>
2025-04-14 19:17:25 +0800

    Our repo for final requirement in websys

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    //guys dito po ilalagay ung mga function na Login, Logout, Register, forgot password
+}
