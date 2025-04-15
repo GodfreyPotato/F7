43e345f0a281b7c45cea8385460cc5cf4320e06e
psuuc-mvgo <Gomichaelangelo69@gmail.com>
2025-04-15 12:22:18 +0800

    First push adding registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\UserRegistration;
 
 class UserController extends Controller
 {
-    //
     //guys dito po ilalagay ung mga function na Login, Logout, Register, forgot password
+
+    // Registration
+    public function showRegistrationForm(){
+        return view('auth.registration');
+    }
+
+    public function register(Request $request){
+        $request->validate([
+        'name'      => 'required|string|max:255',
+        'email'     => 'required|email|unique:user_registrations,email',
+        'password'  => 'required|min:6|confirmed',
+        ]);
+
+        UserRegistration::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('auth.registrationForm')->with('success', 'Registration successful!');
+    }
+
+    // Login
+
+
+    // Logout
+
+    // Forgot Password
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('registration', [UserController::class, 'showRegistrationForm'])->name('auth.registrationForm');
+Route::post('registration',[UserController::class, 'register'])->name('auth.register');
