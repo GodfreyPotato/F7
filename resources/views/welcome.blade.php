daf40ac362c06ac87622aa9991d7eab1b1581ee4
GodfreyPotato <godfreyjavier88@gmail.com>
2025-05-17 01:44:11 +0800

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,5 @@
 {{-- <h1> This is home</h1> --}}
 @extends('master')
 @section('content')
-    @if(auth()->check())
-        @if(auth()->user()->role == 'employee')
-           
-        @elseif(auth()->user()->role == 'admin')
-            
-        @endif
-
-    @else
-        <h1>This is home</h1>
-        <a href="{{route('auth.login')}}">Login Here</a>
-    @endif
+    //fionah gawa you here
 @endsection
