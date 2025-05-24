a5c5371abd83342fc88112daba590ede1ec042fb
itsmefionah <santuafionah.05@gmail.com>
2025-05-25 00:45:58 +0800

    changes

---
+++ resources/views/admin/reviewRequest.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+@section('content')
+    <div class="container" style="margin-top: 5vw;">
+        <div class="d-flex flex-column">
+            <div class="d-flex justify-content-between ">
+                <span style="font-size: 20px;" class="fw-bold">Review Requests</span>
+                    <a href="#" class="btn d-flex justify-content-center"
+                        style="background-color: #059669; color: white;">
+                        Approve
+                    </a>
+            </div>
+        </div>
+    </div>
+@endsection
