a9cf0d394911c3c6b57beaef3c99988a9890022e
GodfreyPotato <godfreyjavier88@gmail.com>
2025-06-04 03:07:35 +0800

    updates on db

--- database/migrations/2025_04_15_130724_users_table.php
+++ database/migrations/2025_04_15_130724_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->enum('role', ['admin', 'ni', 'ins']);
             $table->string('password');
+            $table->string('image_path')->nullable();
             $table->timestamps();
         });
     }
